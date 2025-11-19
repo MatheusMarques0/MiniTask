8383f14fd393890b55009c788ca3e29591d1eba7
NicolasSantana18 <nicolasnss1107fdn@gmail.com>
2025-11-19 14:39:06 -0300

    Formando a tela de deleção

---
+++ deletarContato.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deletar</title>
+    <link rel="stylesheet" href="estilos/deletarCadastro.css">
+</head>
+<body>
+
+    <main>
+        <section id="log">
+            <h1>MiniTask</h1>
+            <h2>Deseja Excluir esse Registro?</h2>
+            <form action="deletarContato.php">
+                <input type="submit" value="Sim" class="deletar">
+                <input type="reset" value="Não" class="deletar">
+            </form>
+        </section>
+    </main>
+
+    
+</body>
+</html>
